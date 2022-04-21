e71aca92bd3da57ec5c565104fac14f2ea68b404
tomerfatael <tomerfatael@mail.tau.ac.il>
2022-04-21 16:22:57 +0300

    added fixes

--- geo_qa.py
+++ geo_qa.py
@@ -1,4 +1,4 @@
-import string
+import sys
 import requests
 import lxml.html
 import rdflib
@@ -34,9 +34,9 @@ def build_country_president(country_doc, ont_country):
     """
     PRESIDENT_OF = rdflib.URIRef("president_of")
     president_name = country_doc.xpath("//table[contains(@class, 'infobox')]//tr[th//a[text()='President']]//td//a[@title][1]//text()")
-    ont_name = rdflib.URIRef(president_name[0].replace(" ", "_"))
     # check if the country have a president
     if len(president_name) > 0:
+        ont_name = rdflib.URIRef(president_name[0].replace(" ", "_"))
         g.add((ont_name, PRESIDENT_OF, ont_country))
 
         presidnet_wiki_suffix = country_doc.xpath("//table[contains(@class, 'infobox')]//tr[th//a[text()='President']]//td//a//@href")
@@ -50,10 +50,11 @@ def build_country_president(country_doc, ont_country):
             ont_date_of_birth = rdflib.URIRef(president_date_of_birth[0])
             g.add((ont_name, DATE_OF_BIRTH, ont_date_of_birth))
 
-        president_place_of_birth = president_doc.xpath("//table//tr[th[text()='Born']]//a//text()")
+        res = president_doc.xpath("//table//tr[th[text()='Born']]//a//text()")
+        president_place_of_birth = [place.replace(" ", "_") for place in res if place[0].isalpha()]
         if len(president_place_of_birth) > 0:
             PLACE_OF_BIRTH = rdflib.URIRef("place_of_birth")
-            ont_place_of_birth = rdflib.URIRef(president_place_of_birth[0])
+            ont_place_of_birth = rdflib.URIRef(",".join(president_place_of_birth))
             g.add((ont_name, PLACE_OF_BIRTH, ont_place_of_birth))
 
 def build_country_prime_minister(country_doc, ont_country):
@@ -64,9 +65,9 @@ def build_country_prime_minister(country_doc, ont_country):
     """
     PRIME_MINISTER_OF = rdflib.URIRef("prime_minister_of")
     prime_minister_name = country_doc.xpath("//table[contains(@class, 'infobox')]//tr[th//a[text()='Prime Minister']]//td//a[@title][1]//text()")
-    ont_name = rdflib.URIRef(prime_minister_name[0].replace(" ", "_"))
     # check if the country have a prime minister
     if len(prime_minister_name) > 0:
+        ont_name = rdflib.URIRef(prime_minister_name[0].replace(" ", "_"))
         g.add((ont_name, PRIME_MINISTER_OF, ont_country))
 
         prime_minister_wiki_suffix = country_doc.xpath("//table[contains(@class, 'infobox')]//tr[th//a[text()='Prime Minister']]//td//a//@href")
@@ -80,10 +81,11 @@ def build_country_prime_minister(country_doc, ont_country):
             ont_date_of_birth = rdflib.URIRef(prime_minister_date_of_birth[0])
             g.add((ont_name, DATE_OF_BIRTH, ont_date_of_birth))
 
-        prime_minister_place_of_birth = prime_minister_doc.xpath("//table//tr[th[text()='Born']]//a//text()")
+        res = prime_minister_doc.xpath("//table//tr[th[text()='Born']]//a//text()")
+        prime_minister_place_of_birth = [place.replace(" ", "_") for place in res if place[0].isalpha()]
         if len(prime_minister_place_of_birth) > 0:
             PLACE_OF_BIRTH = rdflib.URIRef("place_of_birth")
-            ont_place_of_birth = rdflib.URIRef(prime_minister_place_of_birth[0])
+            ont_place_of_birth = rdflib.URIRef(",".join(prime_minister_place_of_birth))
             g.add((ont_name, PLACE_OF_BIRTH, ont_place_of_birth))
 
 def build_country_capital(country_doc, country):
@@ -109,25 +111,43 @@ def build_country_population(country_doc, country):
 def build_country_form_of_government(country_doc, ont_country):
     GOVERNMENT_IN = rdflib.URIRef("government_in")
     res = country_doc.xpath("//table[contains(@class, 'infobox')]//tr[.//a[text()='Government']]//td//text()")
-    form_of_government = [rdflib.URIRef(government.replace(" ", "_")) for government in res if government[0].isalpha()]
-    for government in form_of_government: #maybe need to add all as a list
-        g.add((government, GOVERNMENT_IN, ont_country))
+    form_of_government = [government for government in res if government[0].isalpha()]
+    for government in form_of_government:
+        #add to dict
+
+        g.add((rdflib.URIRef(government.replace(" ", "_")), GOVERNMENT_IN, ont_country))
+
+if sys.argv[1] == 'create':
+    build_countries_url()
+    for country in countries_url:
+        ont_country = rdflib.URIRef(country[1])
+        r = requests.get(country[0])
+        country_doc = lxml.html.fromstring(r)
+        build_country_president(country_doc, ont_country)
+        build_country_prime_minister(country_doc, ont_country)
+        build_country_form_of_government(country_doc, ont_country)
+        #add avi functions
+        g.serialize("ontology.nt", format="nt")
+
+# if sys.argv[1] == 'question':
 
 
 
-# input for tests
-a = requests.get("https://en.wikipedia.org/wiki/China")
+# input for single country tests
+
+a = requests.get("https://en.wikipedia.org/wiki/israel")
 doc = lxml.html.fromstring(a.content)
-ont_country = rdflib.URIRef("china")
+ont_country = rdflib.URIRef("india")
 build_country_president(doc, ont_country)
-build_country_form_of_government(doc, ont_country)
-g.serialize("ontology.nt", format="nt") #after this command u can see the ontology graph in the ontology.nt file
+# g.serialize("ontology.nt", format="nt") #after this command u can see the ontology graph in the ontology.nt file
 
+
+#input for all countries test
 
 # build_countries_url()
 # for c in countries_url:
 #     ont_name = rdflib.URIRef(c[1])
 #     a = requests.get(c[0])
 #     doc = lxml.html.fromstring(a.content)
-#     build_country_form_of_government(doc,ont_name)
+#     build_country_president(doc,ont_name)
 # g.serialize("ontology.nt", format="nt")
