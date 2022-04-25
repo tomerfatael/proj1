830697db25dbca49c510eca83d9c63a48ad8789d
tomerfatael <tomerfatael@mail.tau.ac.il>
2022-04-25 16:52:02 +0300

    move to tomer branch

--- geo_qa.py
+++ geo_qa.py
@@ -6,9 +6,10 @@ import rdflib
 g = rdflib.Graph()
 COUNTRIES = " https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)"
 WIKI_PREFIX = "https://en.wikipedia.org"
+EXAMPLE = "http://example.org/"
 countries_url = []
+relations_dict = {"PRESIDENT_OF" : rdflib.URIRef(EXAMPLE + "president_of")} ##needed??
 government_forms_dict = {}
-EXAMPLE = "http://example.org/" # check if needed with Tomer
 
 
 ## building ontology ##
@@ -24,7 +25,7 @@ def build_countries_url() -> None:
     for country in doc.xpath("//table//tbody//tr//span//a//@href"):
         url = WIKI_PREFIX + country
         country_name = country.split("/")[2]
-        ont_country = rdflib.URIRef(country_name)
+        ont_country = rdflib.URIRef(EXAMPLE + country_name)
         countries_url.append((url, ont_country))
 
 def build_country_president(country_doc, ont_country):
@@ -33,7 +34,7 @@ def build_country_president(country_doc, ont_country):
     in order to get his date of birth and place of birth
     :rtype: None
     """
-    PRESIDENT_OF = rdflib.URIRef("president_of")
+    PRESIDENT_OF = rdflib.URIRef(EXAMPLE + "president_of")
     president_name = country_doc.xpath("//table[contains(@class, 'infobox')]//tr[th//a[text()='President']]//td//a[@title][1]//text()")
     # check if the country have a president
     if len(president_name) > 0:
@@ -47,14 +48,14 @@ def build_country_president(country_doc, ont_country):
         president_doc = lxml.html.fromstring(r.content)
         president_date_of_birth = president_doc.xpath("//table//tr[th[text()='Born']]//span[@class='bday']//text()")
         if len(president_date_of_birth) > 0:
-            DATE_OF_BIRTH = rdflib.URIRef("date_of_birth")
+            DATE_OF_BIRTH = rdflib.URIRef(EXAMPLE + "date_of_birth")
             ont_date_of_birth = rdflib.URIRef(president_date_of_birth[0])
             g.add((ont_name, DATE_OF_BIRTH, ont_date_of_birth))
 
         res = president_doc.xpath("//table//tr[th[text()='Born']]//a//text()")
         president_place_of_birth = [place.replace(" ", "_") for place in res if place[0].isalpha()]
         if len(president_place_of_birth) > 0:
-            PLACE_OF_BIRTH = rdflib.URIRef("place_of_birth")
+            PLACE_OF_BIRTH = rdflib.URIRef(EXAMPLE + "place_of_birth")
             ont_place_of_birth = rdflib.URIRef(",".join(president_place_of_birth))
             g.add((ont_name, PLACE_OF_BIRTH, ont_place_of_birth))
 
@@ -64,7 +65,7 @@ def build_country_prime_minister(country_doc, ont_country):
     in order to get his date of birth and place of birth
     :rtype: None
     """
-    PRIME_MINISTER_OF = rdflib.URIRef("prime_minister_of")
+    PRIME_MINISTER_OF = rdflib.URIRef(EXAMPLE + "prime_minister_of")
     prime_minister_name = country_doc.xpath("//table[contains(@class, 'infobox')]//tr[th//a[text()='Prime Minister']]//td//a[@title][1]//text()")
     # check if the country have a prime minister
     if len(prime_minister_name) > 0:
@@ -78,14 +79,14 @@ def build_country_prime_minister(country_doc, ont_country):
         prime_minister_doc = lxml.html.fromstring(r.content)
         prime_minister_date_of_birth = prime_minister_doc.xpath("//table//tr[th[text()='Born']]//span[@class='bday']//text()")
         if len(prime_minister_date_of_birth) > 0:
-            DATE_OF_BIRTH = rdflib.URIRef("date_of_birth")
+            DATE_OF_BIRTH = rdflib.URIRef(EXAMPLE + "date_of_birth")
             ont_date_of_birth = rdflib.URIRef(prime_minister_date_of_birth[0])
             g.add((ont_name, DATE_OF_BIRTH, ont_date_of_birth))
 
         res = prime_minister_doc.xpath("//table//tr[th[text()='Born']]//a//text()")
         prime_minister_place_of_birth = [place.replace(" ", "_") for place in res if place[0].isalpha()]
         if len(prime_minister_place_of_birth) > 0:
-            PLACE_OF_BIRTH = rdflib.URIRef("place_of_birth")
+            PLACE_OF_BIRTH = rdflib.URIRef(EXAMPLE + "place_of_birth")
             ont_place_of_birth = rdflib.URIRef(",".join(prime_minister_place_of_birth))
             g.add((ont_name, PLACE_OF_BIRTH, ont_place_of_birth))
 
@@ -125,13 +126,19 @@ def build_country_area(country_doc, ont_country):
 
 
 def build_country_form_of_government(country_doc, ont_country):
-    GOVERNMENT_IN = rdflib.URIRef("government_in")
+    GOVERNMENT_IN = rdflib.URIRef(EXAMPLE + "government_in")
     res = country_doc.xpath("//table[contains(@class, 'infobox')]//tr[.//a[text()='Government']]//td//text()")
     form_of_government = [government for government in res if government[0].isalpha()]
     for government in form_of_government:
         #add to dict
 
         g.add((rdflib.URIRef(government.replace(" ", "_")), GOVERNMENT_IN, ont_country))
+
+def build_question(question: str):
+    if question.find("president of") != -1:
+        relation =
+        if question.find("Who") != -1:
+
 
 # ask Tomer if we should send the relations to the build functions, or defining them every build ---
 
